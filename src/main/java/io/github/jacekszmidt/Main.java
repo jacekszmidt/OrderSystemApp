2b65c6d71869655a991b0271680a54372c6161ce
jacekszmidt <github.jacek@gmail.com>
2019-11-12 19:05:06 +0100

    maven configurated

--- src/main/java/io/github/jacekszmidt/Main.java
+++ src/main/java/io/github/jacekszmidt/Main.java
@@ -1,9 +1,11 @@
-package com.company;
+package io.github.jacekszmidt;
+
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
     }
 }
 
